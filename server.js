4a532c824dfbf870d3f1dc39a9ecd92deae630f8
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 07:15:10 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const dotenv = require("dotenv").config();
-const app = require("./app");
 const mongoose = require("mongoose");
+const app = require("./app");
 
-// Connect to Mongoose
 mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
@@ -12,6 +11,5 @@ mongoose
   .then((res) => console.log("Connected to Database"))
   .catch((err) => console.log(err));
 
-// Activate the Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server is runinig on port ${PORT} ...`));
